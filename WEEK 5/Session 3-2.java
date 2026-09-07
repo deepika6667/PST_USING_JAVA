282b313fc29f7dc2b25a0ca6c73799681df2f0d4
deepika6667 <vtu29548@veltech.edu.in>
2026-09-07 10:27:06 +0530

    Add alternatingCharacters function in Session 3-2.java
    Implement a solution to count deletions needed for alternating characters in a string.

---
+++ WEEK 5/Session 3-2.java
@@ -0,0 +1,50 @@
+alternating-characters:
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static int alternatingCharacters(String s) {
+        int deletions = 0;
+        
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                deletions++;
+            }
+        }
+        
+        return deletions;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.hasNextInt()) {
+            int q = scanner.nextInt();
+            while (q-- > 0) {
+                String s = scanner.next();
+                System.out.println(alternatingCharacters(s));
+            }
+        }
+        scanner.close();
+    }
+}
+OUTPUT:
+Input (stdin)
+5
+AAAA
+BBBBB
+ABABABAB
+BABABA
+AAABBB
+Your Output (stdout)
+3
+4
+0
+0
+4
+Expected Output
+3
+4
+0
+0
+4
